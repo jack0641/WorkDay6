52fba449b672359e37622c0f8be876b45ea42bf2
jack0641 <jack0641@umn.edu>
2013-10-09 17:12:03 -0500

    Yay!

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -1,9 +1,14 @@
 ﻿Public Class Form1
 
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
-
+        Dim x As Integer
         lstResult.Items.Clear()
-        lstResult.Items.Add("Not implemented yet")
+
+        Do While (x < 10 ^ 2)
+            x = (x) ^ 0.5 + 1
+            x = x ^ 2
+            lstResult.Items.Add(x)
+        Loop
 
     End Sub
 End Class
--- PopulationSim/PopulationSim/Form1.vb
+++ PopulationSim/PopulationSim/Form1.vb
@@ -3,8 +3,19 @@
 Public Class Form1
 
     Private Sub btnPopulation_Click(sender As System.Object, e As System.EventArgs) Handles btnPopulation.Click
+        Dim curYear As Integer
+        Dim curPop As Double
 
-        MessageBox.Show("Not implemented yet", "Error")
+        curPop = 7000
+        curYear = 2012
+        Do While (curPop > 6)
+            curYear = curYear - 50
+            curPop = curPop / 2
+
+        Loop
+
+
+        MessageBox.Show("The Year Is " & curYear, "Error")
 
     End Sub
 End Class
